60e08ec71ac450ecb384b75914860a6e5abde417
tushar.vaishya <tushar.vaishya@arconnet.com>
2024-11-22 21:06:27 +0530

    Test no 2

--- Common_Repo/obj/Release/net8.0/Common_Repo.AssemblyInfo.cs
+++ Common_Repo/obj/Release/net8.0/Common_Repo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common_Repo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d109c7adff7281af8a313c903539ee9c30acd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ad8896e880401990e3090d895137569a40083c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common_Repo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common_Repo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
